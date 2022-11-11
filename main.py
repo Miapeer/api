9489427b8870e0c16c9a49c68caa4da4976dfad7
Jeff Navarra <jeff.navarra@miapeer.com>
2022-11-11 11:10:12 -0700

    Fix env var loading

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import sys
 
-print(sys.path)
 from dotenv import find_dotenv, load_dotenv
 
 from miapeer.adapter.database import create_db_and_tables
-
-ENV_FILE = find_dotenv()
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
-
 from miapeer.app import app
 
 
