e02e31968b67e6e564004ce50cfadd66ff702413
Jeff Navarra <jeff.navarra@miapeer.com>
2022-11-12 12:32:07 -0700

    Add endpoint for logos

--- miapeer/app.py
+++ miapeer/app.py
@@ -1,13 +1,18 @@
 from os import environ as env
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 
 from miapeer.auth import auth0
 from miapeer.routers import miapeer, miapeer_api
 
 app = FastAPI()
+
 app.add_middleware(SessionMiddleware, secret_key=env.get("APP_SECRET_KEY"))
+
+app.mount("/static", StaticFiles(directory="./miapeer/static"), name="static")
+
 app.include_router(auth0.router)
 app.include_router(miapeer.router)
 app.include_router(miapeer_api.application.router)
