a29b34187a3c18ba8d2a0805789f3413b2e317e1
Jeff Navarra <jeff.navarra@miapeer.com>
2023-01-08 21:54:01 -0700

    Update CORS

--- miapeer/app.py
+++ miapeer/app.py
@@ -13,11 +13,13 @@ if env.get("APP_SECRET_KEY") is None or env.get("JWT_SECRET_KEY") is None:
 
 app.add_middleware(SessionMiddleware, secret_key=env.get("APP_SECRET_KEY"))
 
+# TODO: Update these for prod/dev? Also, Azure WebApp config helps with this.
 origins = [
     "http://www.miapeer.com",
     "https://www.miapeer.com",
     "http://miapeer.azurewebsites.net",
-    "https://miapeer.azurewebsites.net"
+    "https://miapeer.azurewebsites.net",
+    "http://localhost:8001",
 ]
 
 app.add_middleware(
