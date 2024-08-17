64c83cb573570b73c980b2c0c5e969cc5d6c52d9
Miapeer <jeff.navarra@miapeer.com>
2024-08-17 11:11:18 -0600

    Other scheduled transaction methods (#11)
    * Update PATCH and extract method
    
    * Update get all to return next_transaction

--- miapeer/tests/integration/Quantum/test_scheduled_transactions.py
+++ miapeer/tests/integration/Quantum/test_scheduled_transactions.py
@@ -41,7 +41,20 @@ class TestGetAll:
                 "repeat_option_id": my_minimal_scheduled_transaction.repeat_option_id,
                 "notes": my_minimal_scheduled_transaction.notes,
                 "on_autopay": my_minimal_scheduled_transaction.on_autopay,
-                "next_transaction": None,
+                "next_transaction": {
+                    "transaction_type_id": my_minimal_scheduled_transaction.transaction_type_id,
+                    "payee_id": my_minimal_scheduled_transaction.payee_id,
+                    "category_id": my_minimal_scheduled_transaction.category_id,
+                    "amount": my_minimal_scheduled_transaction.fixed_amount,
+                    "transaction_date": my_minimal_scheduled_transaction.start_date.strftime("%Y-%m-%d"),
+                    "clear_date": None,
+                    "check_number": None,
+                    "exclude_from_forecast": False,
+                    "notes": my_minimal_scheduled_transaction.notes,
+                    "transaction_id": 0,
+                    "account_id": my_minimal_scheduled_transaction.account_id,
+                    "balance": None,
+                },
             },
             {
                 "scheduled_transaction_id": my_scheduled_transaction.scheduled_transaction_id,
@@ -58,12 +71,27 @@ class TestGetAll:
                 "repeat_option_id": my_scheduled_transaction.repeat_option_id,
                 "notes": my_scheduled_transaction.notes,
                 "on_autopay": my_scheduled_transaction.on_autopay,
-                "next_transaction": None,
+                "next_transaction": {
+                    "transaction_type_id": my_scheduled_transaction.transaction_type_id,
+                    "payee_id": my_scheduled_transaction.payee_id,
+                    "category_id": my_scheduled_transaction.category_id,
+                    "amount": my_scheduled_transaction.fixed_amount,
+                    "transaction_date": my_scheduled_transaction.start_date.strftime("%Y-%m-%d"),
+                    "clear_date": None,
+                    "check_number": None,
+                    "exclude_from_forecast": False,
+                    "notes": my_scheduled_transaction.notes,
+                    "transaction_id": 0,
+                    "account_id": my_scheduled_transaction.account_id,
+                    "balance": None,
+                },
             },
         ]
 
+        actual = response.json()
+
         assert response.status_code == 200
-        assert response.json() == expected
+        assert actual == expected
 
 
 @pytest.mark.usefixtures("create_complete_portfolio")
@@ -386,7 +414,7 @@ class TestUpdate:
                 date(year=2021, month=2, day=3),
                 date(year=2020, month=1, day=2),
                 8129,
-                3,
+                2,
                 "new note",
                 False,
             ),
@@ -464,7 +492,20 @@ class TestUpdate:
             "repeat_option_id": repeat_option_id,
             "notes": notes,
             "on_autopay": on_autopay,
-            "next_transaction": None,
+            "next_transaction": {
+                "transaction_type_id": getattr(scheduled_transaction, "transaction_type_id", None),
+                "payee_id": getattr(payee, "payee_id", None),
+                "category_id": getattr(category, "category_id", None),
+                "amount": fixed_amount,
+                "transaction_date": start_date.strftime("%Y-%m-%d") if start_date is not None else None,
+                "clear_date": None,
+                "check_number": None,
+                "exclude_from_forecast": False,
+                "notes": notes,
+                "transaction_id": 0,
+                "account_id": scheduled_transaction.account_id,
+                "balance": None,
+            },
         }
 
         actual = response.json()
