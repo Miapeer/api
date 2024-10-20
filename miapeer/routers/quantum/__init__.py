eaefebeda3bfe266b9f4e9716d95665bbf9a4a03
Jep <jeff.navarra@miapeer.com>
2024-10-20 09:18:34 -0600

    Create router

--- miapeer/routers/quantum/__init__.py
+++ miapeer/routers/quantum/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from miapeer.routers.quantum import (
     account,
+    budget,
     category,
     payee,
     portfolio,
@@ -16,11 +17,12 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-router.include_router(portfolio.router)
 router.include_router(account.router)
-router.include_router(payee.router)
-router.include_router(transaction_type.router)
+router.include_router(budget.router)
 router.include_router(category.router)
-router.include_router(transaction.router)
-router.include_router(scheduled_transaction.router)
+router.include_router(payee.router)
+router.include_router(portfolio.router)
 router.include_router(repeat_option.router)
+router.include_router(scheduled_transaction.router)
+router.include_router(transaction.router)
+router.include_router(transaction_type.router)
